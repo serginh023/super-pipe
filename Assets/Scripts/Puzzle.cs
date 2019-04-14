1b44ce3516369fd888f6034964603929af22fd64
serginh023 <psergio.farias@gmail.com>
2019-04-14 07:06:53 -0400

    adicionado script puzzle para controlar o tabuleiro de uma forma geral

---
+++ Assets/Scripts/Puzzle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle : MonoBehaviour
+{
+
+    public int altura = 5;
+    public int largura = 5;
+    
+    private int[,] puzzle;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        puzzle = new int[altura, largura];
+    }
+
+}
