55f34083b1258ed6476c00e36a46e9bf5636e048
serginh023 <psergio.farias@gmail.com>
2019-04-29 01:18:33 -0400

    adicionado eventosteste para aprender como utilizar eventos para quando o jogador começar, finalizar partidas

---
+++ Assets/Scripts/EventosTeste.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class EventosTeste : MonoBehaviour
+{
+    UnityEvent m_MyEvent;
+
+    void Start()
+    {
+        if (m_MyEvent == null)
+            m_MyEvent = new UnityEvent();
+
+        m_MyEvent.AddListener(Ping);
+        m_MyEvent.AddListener(teste);
+    }
+
+    void Update()
+    {
+        if (Input.anyKeyDown && m_MyEvent != null)
+        {
+            m_MyEvent.Invoke();
+        }
+    }
+
+    void Ping()
+    {
+        Debug.Log("Ping");
+    }
+
+    void teste(){
+        Debug.Log("teste");
+    }
+}
